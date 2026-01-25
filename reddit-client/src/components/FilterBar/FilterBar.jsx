21f0c22fcea27b133ada03b51c89f7f6ab98ca2f
Michel Kowal <michelkowal@hotmail.de>
2026-01-25 20:08:46 +0000

    create filterbar component-comments

--- reddit-client/src/components/FilterBar/FilterBar.jsx
+++ reddit-client/src/components/FilterBar/FilterBar.jsx
@@ -1,20 +1,20 @@
 import "./FilterBar.css";
 import { useState } from "react";
 
-const FILTERS = ["hot", "new", "top"];
+const FILTERS = ["hot", "new", "top"]; //Defining a list of Filtervalues as array pattern
 
 function FilterBar() {
-    const [activeFilter, setActiveFilter] = useState("hot");
+    const [activeFilter, setActiveFilter] = useState("hot");  //actual-state is "hot"
 
     return (
         <div className="filter-bar">
-            {FILTERS.map((filter) => (
+            {FILTERS.map((filter) => (      //Creating buttons for each array element => 3 Buttons
                 <button
-                    key={filter}
-                    className={filter === activeFilter? "active" : ""}
-                    onClick={() => setActiveFilter(filter)}
+                    key={filter}            //react needs key attribut to find the right listelement
+                    className={filter === activeFilter? "active" : ""}      //active-filter gets an specified CSS-Class
+                    onClick={() => setActiveFilter(filter)}                 // By clicking => changing the actual filter
                 >
-                    {filter}
+                    {filter}    {/* Text for each Button of the FILTER-Array  */}
                 </button>
             ))}
         </div>
