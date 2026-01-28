ca263cc4c7e12d4715df42b163df6d54655878b9
Michel Kowal <michelkowal@hotmail.de>
2026-01-28 23:12:14 +0000

    Refactor FilterBar: replace useState with Redux useSelector/useDispatch

--- reddit-client/src/components/FilterBar/FilterBar.jsx
+++ reddit-client/src/components/FilterBar/FilterBar.jsx
@@ -1,10 +1,17 @@
 import "./FilterBar.css";
-import { useState } from "react";
 
-const FILTERS = ["hot", "new", "top"];      //Defining a list of Filtervalues as array pattern
+import { useSelector, useDispatch } from "react-redux";
+import { setFilter } from "../../features/filters/filtersSlice";
+
 
 function FilterBar() {
-    const [activeFilter, setActiveFilter] = useState("hot");    //actual-state is "hot"
+
+const dispatch = useDispatch();
+
+    // Redux State auslesen
+const activeFilter = useSelector((state) => state.filters.activeFilter);
+
+const FILTERS = ["hot", "new", "top"];      //Defining a list of Filtervalues as array pattern
 
     return (
         <div className="filter-bar">
@@ -12,7 +19,7 @@ function FilterBar() {
                 <button
                     key={filter}            //react needs key attribut to find the right listelement
                     className={filter === activeFilter? "active" : ""}      //active-filter gets an specified CSS-Class
-                    onClick={() => setActiveFilter(filter)}                 // By clicking => changing the actual filter
+                    onClick={() => dispatch(setFilter(filter))}                 // By clicking => changing the actual filter
                 >
                     {filter}    {/* Text for each Button of the FILTER-Array  */}
                 </button>
